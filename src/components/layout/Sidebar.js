b32f42281c958f014614fdb72a0a105ce4b67cb6
DugourdClement <clement.dugourd@etu.univ-amu.fr>
2024-01-27 22:01:24 +0100

    Sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,8 +1,119 @@
-import {Typography} from "@mui/material";
+import React, {useState} from 'react';
+import {
+    Drawer,
+    List,
+    ListItemText,
+    IconButton,
+    ListItemButton,
+    Typography, Card, Grid
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import {useNavigate} from "react-router-dom";
+
+const navigationItems = [
+    {path: "/Accueil", label: "Accueil"},
+    {path: "/Qui-sommes-nous", label: "Qui sommes-nous ?"},
+    {path: "/Organisation", label: "Organisation"},
+    {path: "/Partenariats/Créateurs", label: "Partenariats / Créateurs"},
+    {path: "/Boutique", label: "Boutique"},
+    {path: "/Billeterie", label: "Billeterie"}
+];
+
+const drawerWidth = '51vh';
 
 const Sidebar = () => {
+    const [open, setOpen] = useState(false);
+
+    const navigate = useNavigate();
+
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
+    const navigationHandler = (target) => {
+        navigate(target)
+    };
+
     return (
-        <Typography>Sidebar</Typography>
+        <>
+            <Card
+                sx={{
+                    width: '15vh',
+                    height: '100vh',
+                    position: 'fixed',
+                    zIndex: 1,
+                    backgroundColor: '#F2E1C7',
+                    borderTop: 'solid',
+                    borderRight: 'solid',
+                    borderColor: 'white',
+                }}
+            >
+                <Grid container direction="column" alignItems="center">
+                    <Grid item sx={{borderBottom: 'solid', borderColor: 'white'}}>
+                        <IconButton onClick={toggleDrawer} sx={{
+                            pt: '4vh',
+                            pb: '4vh',
+                            '&:hover': {backgroundColor: 'transparent'},
+                        }}>
+                            {!open ? <MenuIcon/> : <CloseIcon/>}
+                        </IconButton>
+                    </Grid>
+                    <Grid item>
+                        {!open && <Typography
+                            sx={{
+                                fontSize: 35,
+                                fontWeight: 'bold',
+                                writingMode: 'vertical-rl',
+                                transform: 'rotate(180deg)',
+                                mt: '20vh'
+                            }}>
+                            Somebody Like You
+                        </Typography>}
+                    </Grid>
+                </Grid>
+            </Card>
+            <Drawer
+                anchor="left"
+                open={open}
+                onClose={toggleDrawer}
+                variant="temporary"
+                ModalProps={{
+                    BackdropProps: {
+                        invisible: true,
+                    }
+                }}
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        position: 'relative',
+                        left: open ? '15vh' : `-${drawerWidth}`,
+                        zIndex: 2,
+                        boxSizing: 'border-box',
+                        backgroundColor: '#F2E1C7',
+                        boxShadow: 'none',
+                        borderTop: 'solid',
+                        borderLeft: 'solid',
+                        borderColor: 'white',
+                    },
+                }}>
+                <Typography variant="h4" sx={{ml: '2vh', mt: '10vh', mb: '4vh', fontWeight: 'bold'}}>
+                    Somebody Like You
+                </Typography>
+                <List>
+                    {navigationItems.map((item) => (
+                        <ListItemButton key={item.path} onClick={() => navigationHandler(item.path)}>
+                            <ListItemText
+                                sx={{'& .MuiListItemText-primary': {fontSize: 28, fontWeight: 'bold', color: 'gray'}}}
+                                primary={item.label}
+                            />
+                        </ListItemButton>
+                    ))}
+                </List>
+            </Drawer>
+        </>
     );
 };
 
